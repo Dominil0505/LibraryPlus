798321c7fac8adacfd5fa62407f760372360d090
Dominik <dominik.szabo.03@gmail.com>
2025-05-17 15:15:52 +0200

    user and book model, migrations

--- book/models.py
+++ book/models.py
@@ -1,3 +1,36 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class BaseModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(null=False, db_index=True)
+    
+    class Meta:
+        abstract = True
+    
+    def __str__(self):
+        return self.name
+
+class Publisher(BaseModel):
+    pass
+
+class Category(BaseModel):
+    pass
+
+class Author(BaseModel):
+    pass
+
+class Book(BaseModel):
+    name = models.CharField(null=False, unique=True, db_index=True)
+    released_date = models.DateTimeField(null=False)
+    copy_number = models.PositiveSmallIntegerField(null=False)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE, null=False)
+    categories = models.ManyToManyField(Category, related_name="books")
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    fee = models.PositiveSmallIntegerField()
+    
+    def __str__(self):
+        return self.name
+    
--- user/models.py
+++ user/models.py
@@ -1,3 +1,48 @@
+from time import timezone
+import uuid
 from django.db import models
 
-# Create your models here.
+from book.models import Book
+
+class BaseModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        abstract = True
+
+class Role(models.Model):
+    role_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    role_name = models.CharField(unique=True)
+    
+    def __str__(self):
+        return self.role_name()
+
+class User(BaseModel):
+    name = models.CharField(null=False)
+    email_address = models.EmailField(unique=True, db_index=True, null=False)
+    role = models.ForeignKey(Role, on_delete=models.PROTECT, null=False)
+    borrow_limit = models.SmallIntegerField(default=5)
+        
+    def __str__(self):
+        return self.name
+
+class Review(models.Model):
+    review_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, null=False)
+    rating = models.PositiveSmallIntegerField(null=False)
+    comment = models.CharField(max_length=256, null=True)
+
+class Borrow(models.Model):
+    borrow_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user_id = models.ManyToManyField(User, related_name="borrows")
+    book_id = models.ForeignKey(Book, on_delete=models.CASCADE, null=False)
+    borrow_date = models.DateTimeField(auto_now_add=True)
+    expiration_date = models.DateTimeField()
+    
+class BorrowFee(models.Model):
+    borrow_fee_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    debt = models.PositiveIntegerField(null=False)
